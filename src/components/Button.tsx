1fcbaa2480e42b09ab50e2d6767d47761ec20c5e
Dana <dana.hong@hotmail.com>
2023-07-24 17:48:32 -0400

    Maint: Update button types

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,23 +1,33 @@
 import RightArrow from "./icons/RightArrow";
 
+type Link = {
+  link: true;
+  path: string;
+};
+
+type Button = {
+  link: false;
+  path: false;
+};
+
 type ButtonType = {
-  link?: boolean;
   variant?: "secondary" | "tertiary";
   children: React.ReactNode;
   className?: string;
-};
+} & (Link | Button);
 
-export default function Button({ className, children, link, variant }: ButtonType) {
+export default function Button({ className, children, path, link, variant }: ButtonType) {
   if (link) {
     return (
       <a
-        className={`flex w-fit items-center justify-center py-4 px-8 text-sm uppercase ${
+        className={`flex w-fit cursor-pointer items-center justify-center py-4 px-8 text-sm uppercase ${
           variant === "secondary"
             ? "border border-black bg-transparent text-black hover:bg-black hover:text-white"
             : variant === "tertiary"
             ? "bg-none hover:text-orange"
             : "bg-orange text-white hover:bg-activeOrange"
         } ${className}`}
+        href={path}
       >
         {children}
       </a>
