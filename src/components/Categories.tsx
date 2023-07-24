0849d82c3ff5a19c8acaa84caf4b4b9b7fe9763e
Dana <dana.hong@hotmail.com>
2023-07-24 14:52:54 -0400

    Update categories preview

--- src/components/Categories.tsx
+++ src/components/Categories.tsx
@@ -5,11 +5,11 @@ import Button from "./Button";
 
 export default function Categories() {
   return (
-    <section className="flex min-[1150px]:px-0 mx-auto w-full max-w-[1110px] flex-col items-center justify-evenly gap-4 px-6 pt-12 pb-[9.375rem] text-center text-[15px] font-bold uppercase md:flex-row md:px-10">
+    <section className="mx-auto mt-12 mb-[9.375rem] flex w-full max-w-[1110px] flex-col items-center justify-evenly gap-4 px-6 text-center text-[15px] font-bold uppercase md:flex-row md:px-10 min-[1150px]:px-0">
       <div
-        className={`flex min-[1110px]:max-w-[350px] relative w-full max-w-[327px] flex-col gap-4 pt-[52px] text-center font-bold md:max-w-[223px] md:pt-20`}
+        className={`relative flex w-full max-w-[327px] flex-col gap-4 pt-[52px] text-center font-bold md:max-w-[223px] md:pt-20 min-[1110px]:max-w-[350px]`}
       >
-        <div className="flex min-[1110px]:pt-[116px] w-full flex-col items-center rounded-lg bg-gray pb-2 pt-[88px]">
+        <div className="flex w-full flex-col items-center rounded-lg bg-gray pb-2 pt-[88px] min-[1110px]:pt-[116px]">
           <span className="block text-[15px] tracking-[1px]">Headphones</span>
           <div className="flex items-center justify-center gap-[14px]">
             <Button variant="tertiary">
@@ -20,13 +20,13 @@ export default function Categories() {
         <img
           src={Headphones}
           alt="Headphones"
-          className="min-[1110px]:-translate-y-16 absolute top-0 order-first -translate-y-1/4 scale-50 md:translate-y-1 md:scale-75"
+          className="absolute top-0 order-first -translate-y-1/4 scale-50 md:translate-y-1 md:scale-75 min-[1110px]:-translate-y-16"
         />
       </div>
       <div
-        className={`flex min-[1110px]:max-w-[350px] relative w-full max-w-[327px] flex-col gap-4 pt-[52px] text-center font-bold md:max-w-[223px] md:pt-20`}
+        className={`relative flex w-full max-w-[327px] flex-col gap-4 pt-[52px] text-center font-bold md:max-w-[223px] md:pt-20 min-[1110px]:max-w-[350px]`}
       >
-        <div className="flex min-[1110px]:pt-[116px] w-full flex-col items-center rounded-lg bg-gray pb-2 pt-[88px]">
+        <div className="flex w-full flex-col items-center rounded-lg bg-gray pb-2 pt-[88px] min-[1110px]:pt-[116px]">
           <span className="block text-[15px] tracking-[1px]">Speakers</span>
           <div className="flex items-center justify-center gap-[14px]">
             <Button variant="tertiary">
@@ -37,13 +37,13 @@ export default function Categories() {
         <img
           src={Speakers}
           alt="Speakers"
-          className="min-[1110px]:-translate-y-16 absolute top-0 order-first -translate-y-1/4 scale-50 md:translate-y-1 md:scale-75"
+          className="absolute top-0 order-first -translate-y-1/4 scale-50 md:translate-y-1 md:scale-75 min-[1110px]:-translate-y-16"
         />
       </div>
       <div
-        className={`flex min-[1110px]:max-w-[350px] relative w-full max-w-[327px] flex-col gap-4 pt-[52px] text-center font-bold md:max-w-[223px] md:pt-20`}
+        className={`relative flex w-full max-w-[327px] flex-col gap-4 pt-[52px] text-center font-bold md:max-w-[223px] md:pt-20 min-[1110px]:max-w-[350px]`}
       >
-        <div className="flex min-[1110px]:pt-[116px] w-full flex-col items-center rounded-lg bg-gray pb-2 pt-[88px]">
+        <div className="flex w-full flex-col items-center rounded-lg bg-gray pb-2 pt-[88px] min-[1110px]:pt-[116px]">
           <span className="block text-[15px] tracking-[1px]">Earphones</span>
           <div className="flex items-center justify-center gap-[14px]">
             <Button variant="tertiary">
@@ -54,7 +54,7 @@ export default function Categories() {
         <img
           src={Earphones}
           alt="Earphone"
-          className="min-[1110px]:-translate-y-16 absolute top-0 order-first -translate-y-1/4 scale-50 md:translate-y-1 md:scale-75"
+          className="absolute top-0 order-first -translate-y-1/4 scale-50 md:translate-y-1 md:scale-75 min-[1110px]:-translate-y-16"
         />
       </div>
     </section>
--- src/components/CategoryPreview.tsx
+++ src/components/CategoryPreview.tsx
@@ -1,29 +1,31 @@
+import Button from "./Button";
 import type { Category } from "../types";
-import arrow from "../assets/shared/desktop/icon-arrow-right.svg";
 
-const CategoryPreview = (props: {
-      image: string;
-      text: Category;
-      height?: string;
-      additional?: string;
-}) => {
-      return (
-            <div className={`flex flex-col gap-4 text-center font-bold ${props.additional}`}>
-                  <span className="text-[15px] tracking-[1px]">{props.text}</span>
-                  <div className="flex items-center justify-center gap-[14px]">
-                        <span className="text-[13px] tracking-[1px] opacity-50">Shop</span>
-                        <img
-                              src={arrow}
-                              alt=""
-                        />
-                  </div>
-                  <img
-                        src={props.image}
-                        alt=""
-                        className={`order-first ${props.height}`}
-                  />
-            </div>
-      );
+type CategoyPreviewProps = {
+  image: string;
+  text: Category;
+  height?: string;
+  additional?: string;
 };
 
-export default CategoryPreview;
+export default function CategoryPreview({
+  image,
+  text,
+  height,
+  additional,
+}: CategoyPreviewProps) {
+  return (
+    <div
+      className={`flex flex-col gap-4 text-center font-bold ${additional} w-full bg-red-300`}
+    >
+      <div></div>
+      <span className="text-[15px] tracking-[1px]">{text}</span>
+      <div className="flex items-center justify-center gap-[14px]">
+        <Button variant="tertiary">
+          <span className="tracking-[1px] opacity-50">Shop</span>
+        </Button>
+      </div>
+      <img src={image} alt="" className={`order-first`} />
+    </div>
+  );
+}
