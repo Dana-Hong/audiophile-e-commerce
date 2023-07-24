9b2086c65a5146c208fc91a83a4267955d37a5ac
Dana <dana.hong@hotmail.com>
2023-07-24 15:28:46 -0400

    maint: fix hero image sizing

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,24 +1,20 @@
 import Button from "./Button";
-import HeroMobile from "../assets/home/mobile/image-header.jpg";
-import HeroTablet from "../assets/home/tablet/image-header.jpg";
 
 export default function Hero() {
   return (
-    <section className="bg-black text-center text-white min-[1110px]:text-left">
-      <div className="min-[400px]:bg-hero-tablet relative mx-auto w-full max-w-[1110px] bg-hero-mobile bg-cover bg-center bg-no-repeat min-[768px]:bg-hero-desktop">
-        <div className="flex flex-col items-center gap-4 py-28 min-[1110px]:items-start">
-          <h2 className="text-sm uppercase tracking-[10px] opacity-50">
-            New Product
-          </h2>
+    <section className="mx-auto max-w-[1440px] bg-hero-mobile bg-cover bg-bottom bg-no-repeat text-center text-white min-[400px]:bg-hero-tablet min-[1110px]:bg-hero-desktop min-[1110px]:text-left">
+      <div className="mx-auto w-full max-w-[1110px] py-28 min-[400px]:py-32">
+        <div className="flex flex-col items-center gap-4 min-[1110px]:items-start">
+          <h2 className="text-sm uppercase tracking-[10px] opacity-50">New Product</h2>
           <h1 className="z-10 text-4xl font-bold md:max-w-[396px] md:text-[56px] md:leading-[58px] md:tracking-[2px]">
             XX99 MARK II HEADPHONES
           </h1>
           <p className="max-w-[350px] pt-2 pb-3 text-[15px] font-medium leading-6 opacity-75">
-            Experience natural, lifelike audio and exceptional build quality
-            made for the passionate music enthusiast.
+            Experience natural, lifelike audio and exceptional build quality made for the passionate
+            music enthusiast.
           </p>
           <Button className="z-10">See product</Button>
-        </div>{" "}
+        </div>
       </div>
     </section>
   );
