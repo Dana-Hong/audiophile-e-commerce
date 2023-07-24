a8fb87c100b8aa1861c3eeeefad7dc14416f2bc1
Dana <dana.hong@hotmail.com>
2023-07-24 17:48:20 -0400

    Maint: Add checkout cart component to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,45 +2,54 @@ import { useState } from "react";
 
 import Hamburger from "./icons/Hamburger";
 import ShoppingCart from "./icons/ShoppingCart";
+import Cart from "./Cart";
 import Logo from "./Logo";
 
 export default function Header() {
   const [navOpen, setNavOpen] = useState(false);
+  const [cartOpen, setCartOpen] = useState(false);
 
   function handleBurgerClick() {
     setNavOpen(!navOpen);
   }
 
+  function handleCartClick() {
+    setCartOpen(!cartOpen);
+  }
+
   return (
-    <header className="relative mx-auto w-full max-w-[1440px] bg-black text-white">
-      <div className="mx-auto flex w-full max-w-[1110px] items-center justify-between border-b border-b-zinc-600 py-4">
-        <button onClick={handleBurgerClick} className="cursor-pointer p-4 md:hidden">
-          <Hamburger />
-        </button>
-        <div className="hidden items-center gap-11 md:flex min-[1110px]:hidden">
-          <button onClick={handleBurgerClick} className="cursor-pointer p-4">
+    <header className="relative mx-auto w-full max-w-[1440px] bg-black">
+      <div className="relative mx-auto max-w-[1110px] border-b border-b-zinc-600 py-4">
+        <div className="mx-auto flex items-center justify-between text-white">
+          <button onClick={handleBurgerClick} className="cursor-pointer p-4 md:hidden">
             <Hamburger />
           </button>
-          <Logo className="cursor-pointer" />
+          <div className="hidden items-center gap-11 md:flex min-[1110px]:hidden">
+            <button onClick={handleBurgerClick} className="cursor-pointer p-4">
+              <Hamburger />
+            </button>
+            <Logo className="cursor-pointer" />
+          </div>
+          <Logo className="md:hidden min-[1110px]:block" />
+          <nav className="hidden gap-8 text-sm font-bold uppercase leading-6 tracking-[2px] min-[1110px]:flex">
+            <a href="/" className="hover:text-orange">
+              Home
+            </a>
+            <a href="/headphones" className="hover:text-orange">
+              Headphones
+            </a>
+            <a href="/speakers" className="hover:text-orange">
+              Speakers
+            </a>
+            <a href="/earphones" className="hover:text-orange">
+              Earphones
+            </a>
+          </nav>
+          <button onClick={handleCartClick} className="cursor-pointer p-4">
+            <ShoppingCart className="fill-white hover:fill-orange" />
+          </button>
         </div>
-        <Logo className="md:hidden min-[1110px]:block" />
-        <nav className="hidden gap-8 text-sm font-bold uppercase leading-6 tracking-[2px] min-[1110px]:flex">
-          <a href="/" className="hover:text-orange">
-            Home
-          </a>
-          <a href="/headphones" className="hover:text-orange">
-            Headphones
-          </a>
-          <a href="/speakers" className="hover:text-orange">
-            Speakers
-          </a>
-          <a href="/earphones" className="hover:text-orange">
-            Earphones
-          </a>
-        </nav>
-        <button className="cursor-pointer p-4">
-          <ShoppingCart className="fill-white hover:fill-orange" />
-        </button>
+        {cartOpen && <Cart />}
       </div>
       <div
         className={`${
