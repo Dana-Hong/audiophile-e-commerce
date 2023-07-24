67fdb435e45afeaec4c1629c43694e5d3fe19bc0
Dana <dana.hong@hotmail.com>
2023-07-24 15:16:14 -0400

    Add missing parenthesis in path

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -18,14 +18,11 @@ module.exports = {
       backgroundImage: {
         "pattern-circles": "url('/assets/home/desktop/pattern-circles.svg')",
         "hero-mobile": "url('/assets/home/mobile/image-header.jpg')",
-        "hero-tablet": "url('/assets/home/tablet/image-header.jpg'",
+        "hero-tablet": "url('/assets/home/tablet/image-header.jpg')",
         "hero-desktop": "url('/assets/home/desktop/image-hero.jpg')",
-        "home-mobile-preview-zx7":
-          "url('/assets/home/mobile/image-speaker-zx7.jpg')",
-        "home-tablet-preview-zx7":
-          "url('/assets/home/tablet/image-speaker-zx7.jpg')",
-        "home-desktop-preview-zx7":
-          "url('/assets/home/desktop/image-speaker-zx7.jpg')",
+        "home-mobile-preview-zx7": "url('/assets/home/mobile/image-speaker-zx7.jpg')",
+        "home-tablet-preview-zx7": "url('/assets/home/tablet/image-speaker-zx7.jpg')",
+        "home-desktop-preview-zx7": "url('/assets/home/desktop/image-speaker-zx7.jpg')",
       },
     },
   },
