9d7f0378514f1a145f6002bd12203753a98f942d
Dana <dana.hong@hotmail.com>
2023-07-24 15:16:30 -0400

    Fix attributes

--- src/components/icons/RightArrow.tsx
+++ src/components/icons/RightArrow.tsx
@@ -1,18 +1,7 @@
 export default function RightArrow({ className }: { className?: string }) {
   return (
-    <svg
-      width="8"
-      height="12"
-      xmlns="http://www.w3.org/2000/svg"
-      className={className}
-    >
-      <path
-        d="M1.322 1l5 5-5 5"
-        stroke="#D87D4A"
-        stroke-width="2"
-        fill="none"
-        fill-rule="evenodd"
-      />
+    <svg width="8" height="12" xmlns="http://www.w3.org/2000/svg" className={className}>
+      <path d="M1.322 1l5 5-5 5" stroke="#D87D4A" strokeWidth="2" fill="none" fillRule="evenodd" />
     </svg>
   );
 }
