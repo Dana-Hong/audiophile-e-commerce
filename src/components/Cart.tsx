9d8fc91fcf566300dfa4ff2090251f4f856b9361
Dana <dana.hong@hotmail.com>
2023-07-24 14:53:30 -0400

    Add home and cart components

---
+++ src/components/Cart.tsx
@@ -0,0 +1,55 @@
+import Button from "./Button";
+import xx99mkiiThumb from "../assets/product-xx99-mark-two-headphones/mobile/image-product.jpg";
+
+export default function Cart() {
+  return (
+    <div className="min-h-screen bg-black bg-opacity-80 pt-6 md:pt-8">
+      <div className="mx-6 flex max-w-sm flex-col gap-8 rounded-lg bg-white px-7 py-8">
+        <div className="flex justify-between">
+          <p className="text-lg font-bold uppercase">Cart (3)</p>
+          <button className="underline opacity-50">Remove all</button>
+        </div>
+        <div className="flex flex-col gap-6">
+          <div className="flex items-center gap-4">
+            <img src={xx99mkiiThumb} alt="" className="h-16 w-16 rounded-lg" />
+            <div className="flex grow flex-col">
+              <div className="flex justify-between">
+                <p className="font-bold">XX99 MKII</p>
+                <p className="opacity-50">x1</p>
+              </div>
+              <p className="text-sm opacity-50">$ 2,999</p>
+            </div>
+          </div>
+          <div className="flex items-center gap-4">
+            <img src={xx99mkiiThumb} alt="" className="h-16 w-16 rounded-lg" />
+            <div className="flex grow flex-col">
+              <div className="flex justify-between">
+                <p className="font-bold">XX99 MKII</p>
+                <p className="text-sm opacity-50">x1</p>
+              </div>
+              <p className="text-sm opacity-50">$ 2,999</p>
+            </div>
+          </div>
+          <div className="flex items-center gap-4">
+            <img src={xx99mkiiThumb} alt="" className="h-16 w-16 rounded-lg" />
+            <div className="flex grow flex-col">
+              <div className="flex justify-between">
+                <p className="font-bold">XX99 MKII</p>
+                <p className="text-sm opacity-50">x1</p>
+              </div>
+              <p className="text-sm opacity-50">$ 2,999</p>
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-col gap-6">
+          <div className="flex justify-between">
+            <p className="uppercase opacity-50">Total</p>
+            <p className="text-lg font-bold">$ 5,396</p>
+          </div>
+
+          <Button className="self-center">Checkout</Button>
+        </div>
+      </div>
+    </div>
+  );
+}
