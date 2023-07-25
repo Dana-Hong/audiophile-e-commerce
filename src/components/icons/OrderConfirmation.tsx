1128c3f39f484627ac36fa328d19212a88c12ae9
Dana <dana.hong@hotmail.com>
2023-07-25 11:44:17 -0400

    Feat: add order confirmation icon

---
+++ src/components/icons/OrderConfirmation.tsx
@@ -0,0 +1,10 @@
+export default function OrderConfirmation({ className }: { className?: string }) {
+  return (
+    <svg width="64" height="64" xmlns="http://www.w3.org/2000/svg">
+      <g fill="none" fillRule="evenodd">
+        <circle fill="#D87D4A" cx="32" cy="32" r="32" />
+        <path stroke="#FFF" strokeWidth="4" d="m20.754 33.333 6.751 6.751 15.804-15.803" />
+      </g>
+    </svg>
+  );
+}
