c773c4ec459529b3405987bc5b711d5319e061a8
Dana <dana.hong@hotmail.com>
2023-07-25 11:43:56 -0400

    Feat: add Link component and update button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,41 +1,17 @@
 import RightArrow from "./icons/RightArrow";
 
-type Link = {
-  link: true;
-  path: string;
-};
-
-type Button = {
-  link: false;
-  path: false;
-};
-
 type ButtonType = {
+  onClick?: () => void;
+  // onClick?: React.Dispatch<React.SetStateAction<boolean>>;
   variant?: "secondary" | "tertiary";
   children: React.ReactNode;
   className?: string;
-} & (Link | Button);
+};
 
-export default function Button({ className, children, path, link, variant }: ButtonType) {
-  if (link) {
-    return (
-      <a
-        className={`flex w-fit cursor-pointer items-center justify-center py-4 px-8 text-sm uppercase ${
-          variant === "secondary"
-            ? "border border-black bg-transparent text-black hover:bg-black hover:text-white"
-            : variant === "tertiary"
-            ? "bg-none hover:text-orange"
-            : "bg-orange text-white hover:bg-activeOrange"
-        } ${className}`}
-        href={path}
-      >
-        {children}
-      </a>
-    );
-  }
-
+export default function Button({ onClick, className, children, variant }: ButtonType) {
   return (
     <button
+      onClick={onClick}
       className={`flex w-fit items-center justify-center py-4 px-8 text-sm uppercase ${
         variant === "secondary"
           ? "border border-black bg-transparent text-black hover:bg-black hover:text-white"
