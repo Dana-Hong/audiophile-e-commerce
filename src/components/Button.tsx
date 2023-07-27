c5c2b96d4bbd8908343960880918abfe0413e390
Dana <dana.hong@hotmail.com>
2023-07-27 10:28:30 -0400

    Resolve button style conflicts with tailwind merge

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,27 +1,41 @@
-import RightArrow from "./icons/RightArrow";
+import { twMerge } from "tailwind-merge";
 
 type ButtonType = {
   onClick?: () => void;
-  // onClick?: React.Dispatch<React.SetStateAction<boolean>>;
   variant?: "secondary" | "tertiary";
   children: React.ReactNode;
   className?: string;
 };
 
 export default function Button({ onClick, className, children, variant }: ButtonType) {
+  function generateVariantStyles(variant: "secondary" | "tertiary" | undefined) {
+    switch (variant) {
+      case "secondary": {
+        return "border border-black bg-transparent text-black hover:bg-black hover:text-white";
+      }
+
+      case "tertiary": {
+        return "hover:text-orange";
+      }
+
+      default: {
+        return "bg-orange text-white hover:bg-activeOrange";
+      }
+    }
+  }
+
+  const styles = generateVariantStyles(variant);
+
   return (
     <button
       onClick={onClick}
-      className={`flex w-fit items-center justify-center py-4 px-8 text-sm uppercase ${
-        variant === "secondary"
-          ? "border border-black bg-transparent text-black hover:bg-black hover:text-white"
-          : variant === "tertiary"
-          ? "bg-none hover:text-orange"
-          : "bg-orange text-white hover:bg-activeOrange"
-      } ${className}`}
+      className={twMerge(
+        "flex w-fit items-center justify-center py-4 px-8 text-sm font-bold uppercase tracking-[1px]",
+        styles,
+        className
+      )}
     >
       {children}
-      {variant === "tertiary" && <RightArrow className="ml-2" />}
     </button>
   );
 }
