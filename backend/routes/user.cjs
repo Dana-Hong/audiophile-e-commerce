6032a4a1da72ce27203ad7b59bc574aa078a4fa0
Dana <dana.hong@hotmail.com>
2023-07-31 15:44:09 -0400

    Feat: add user routes

---
+++ backend/routes/user.cjs
@@ -0,0 +1,11 @@
+const express = require("express");
+const { signupUser, loginUser } = require("../controllers/userController.cjs");
+const router = express.Router();
+
+// login route
+router.post("/login", loginUser);
+
+// signup route
+router.post("/signup", signupUser);
+
+module.exports = router;
