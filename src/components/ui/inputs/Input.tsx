6bf12a68f44ac286c6368714989f2bc11f908d02
Dana <dana.hong@hotmail.com>
2023-08-03 00:13:25 -0400

    Maint: add value to input and fix types

--- src/components/ui/inputs/Input.tsx
+++ src/components/ui/inputs/Input.tsx
@@ -1,20 +1,38 @@
+import { FormData } from "../../../routes/checkout";
+import { LoginForm } from "../../../routes/login";
+import { SignupForm } from "../../../routes/signup";
+
 type InputProps = {
   className?: string;
   name: string;
   placeholder: string;
-  type: "email" | "text" | "tel";
+  type: "email" | "text" | "tel" | "password";
+  id: keyof FormData | keyof LoginForm | keyof SignupForm;
+  value: string;
+  handleChange: (e: React.ChangeEvent<HTMLInputElement>, id: string) => void;
 };
-export default function Input({ className, name, placeholder, type }: InputProps) {
+
+export default function Input({
+  value,
+  id,
+  name,
+  placeholder,
+  type,
+  handleChange,
+  className,
+}: InputProps) {
   return (
     <div className="flex w-full grow flex-col gap-2 md:max-w-[305px]">
-      <label htmlFor="input" className="text-xs font-bold">
+      <label htmlFor={id} className="text-xs font-bold">
         {name}
       </label>
       <input
-        id="input"
+        id={id}
         type={type}
         placeholder={placeholder}
-        className="rounded-lg border border-gray px-6 py-4 text-sm"
+        value={value}
+        className={`rounded-lg border  border-gray px-6 py-4 text-sm focus:outline-4 focus:outline-gray ${className}`}
+        onChange={(e) => handleChange(e, id)}
       />
     </div>
   );
