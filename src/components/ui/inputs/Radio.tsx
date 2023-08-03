75982c6c5658acc5a19cf2f81872b082849b214a
Dana <dana.hong@hotmail.com>
2023-08-03 00:14:26 -0400

    Maint: remove extra div so label can be selected

--- src/components/ui/inputs/Radio.tsx
+++ src/components/ui/inputs/Radio.tsx
@@ -1,15 +1,33 @@
+import { FormData } from "../../../routes/checkout";
+
 type RadioProps = {
   className?: string;
   name: string;
+  label: string;
   id: string;
+  formData: FormData;
+  setFormData: React.Dispatch<React.SetStateAction<FormData>>;
 };
-export default function Radio({ className, name, id }: RadioProps) {
+
+export default function Radio({ className, label, formData, setFormData, name, id }: RadioProps) {
+  const checked = formData[name as keyof FormData] === id.toLocaleLowerCase();
+  function handleClick(id: keyof FormData) {
+    setFormData((fd) => ({ ...fd, [name]: id }));
+  }
   return (
-    <div className="flex max-w-[305px] grow gap-2 rounded-lg border border-gray px-4 py-[18px] accent-orange">
-      <input type="radio" id={id} name={name} />
-      <label htmlFor={id} className="text-sm">
-        {id}
-      </label>
-    </div>
+    <label
+      htmlFor={id}
+      className={`flex max-w-[305px] grow gap-2 rounded-lg border border-gray px-4 py-[18px] accent-orange ${className}`}
+    >
+      <input
+        type="radio"
+        id={id}
+        name={name}
+        checked={checked}
+        value={formData[id as keyof FormData]}
+        onChange={() => handleClick(id as keyof FormData)}
+      />
+      <span className="text-sm">{label}</span>
+    </label>
   );
 }
