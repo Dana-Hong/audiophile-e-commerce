2597fcc559b551931c1de6c26133cdcde53af982
Dana <dana.hong@hotmail.com>
2023-08-03 16:49:02 -0400

    Maint: update default state and CartContext provider

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,9 +1,12 @@
 import { Outlet } from "react-router-dom";
+import { AuthContext } from "../context/AuthContext";
 import { CartContext } from "../context/CartContext";
-import { CheckoutContext } from "../context/CheckoutContext";
 import { useState } from "react";
 import Header from "../components/ui/Header";
 import Footer from "../components/ui/Footer";
+import { Modal, ModalContext } from "../context/ModalContext";
+
+export type UserType = { email: string; token: string };
 
 export type CheckoutItem = {
   name: string;
@@ -15,19 +18,35 @@ export type CheckoutItem = {
   quantity: number;
 };
 
+export type CartType = {
+  items: CheckoutItem[];
+  user_id: string;
+};
+
 export default function Root() {
-  const [checkout, setCheckout] = useState<CheckoutItem[]>([]);
-  const [cartModal, setCartModal] = useState<boolean>(false);
+  const lsUser = localStorage.getItem("user");
+  const lsCart = localStorage.getItem("cart");
+  const [user, setUser] = useState<UserType>(
+    lsUser ? JSON.parse(localStorage.getItem("user") as string) : { email: "", token: "" }
+  );
+  const [modal, setModal] = useState<Modal>({ cart: false, orderConfirm: false });
+  const [cart, setCart] = useState<CartType>(
+    lsCart ? JSON.parse(localStorage.getItem("cart") as string) : { items: [], user_id: "" }
+  );
 
   return (
     <>
-      <CartContext.Provider value={{ cartModal, setCartModal }}>
-        <CheckoutContext.Provider value={{ checkout, setCheckout }}>
-          <Header />
-          <Outlet />
-          <Footer />
-        </CheckoutContext.Provider>
-      </CartContext.Provider>
+      <AuthContext.Provider value={{ user, setUser }}>
+        <ModalContext.Provider value={{ modal, setModal }}>
+          <CartContext.Provider value={{ cart, setCart }}>
+            <div className="flex flex-col">
+              <Header />
+              <Outlet />
+              <Footer />
+            </div>
+          </CartContext.Provider>
+        </ModalContext.Provider>
+      </AuthContext.Provider>
     </>
   );
 }
