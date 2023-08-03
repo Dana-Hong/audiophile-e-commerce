1cdfec92cc6e4f847ab2532ee429db57e187bea3
Dana <dana.hong@hotmail.com>
2023-08-03 00:12:33 -0400

    Maint: update component to use useModal hook

--- src/components/modals/OrderConfirmation.tsx
+++ src/components/modals/OrderConfirmation.tsx
@@ -1,42 +1,75 @@
-import { Link } from "react-router-dom";
+// libraries
+import { useContext } from "react";
+
+// hooks / context
+import useModalContext from "../../hooks/useOrderConfirmContext";
+import { CheckoutContext } from "../../context/CheckoutContext";
+
+// components
+import Button from "../ui/Button";
+
+// icons
 import OrderConfirmation from "../icons/OrderConfirmation";
-import xx99mkiiThumb from "../../assets/product-xx99-mark-two-headphones/mobile/image-product.jpg";
+
+import { formatPrice } from "../../utils/utils";
 
 export default function OrderConfirmModal() {
+  const { setModal } = useModalContext();
+  const { checkout } = useContext(CheckoutContext);
+  const firstItem = checkout[0];
+  const { shortname, image, price, quantity } = firstItem;
+
+  const subTotal = formatPrice(
+    checkout.reduce((acc, curr) => (acc + curr.price) * curr.quantity, 0)
+  );
+
+  const formattedPrice = formatPrice(price);
+  function handleBackgroundClick() {
+    setModal({ cart: false, orderConfirm: false });
+  }
+
+  function handleModalClick(e: React.MouseEvent<HTMLDivElement, MouseEvent>) {
+    e.preventDefault();
+  }
+
   return (
-    <div className="absolute z-10 bg-black bg-opacity-70 px-6">
-      <div className="mx-auto w-full max-w-[540px] rounded-lg bg-white p-8">
+    <div
+      onClick={handleBackgroundClick}
+      className="fixed z-30 min-h-screen w-full bg-zinc-900 bg-opacity-60 pt-6 md:pt-8"
+    >
+      <div
+        onClick={(e) => handleModalClick(e)}
+        className="mx-auto w-full max-w-[540px] rounded-lg bg-white p-8 md:p-12"
+      >
         <OrderConfirmation />
         <p className="mt-8 text-2xl font-bold uppercase">Thank you for your order</p>
         <p className="mt-6 opacity-50">You will receive an email confirmation shortly.</p>
         <div className="mt-8 flex flex-col min-[500px]:flex-row">
-          <div className="flex grow flex-col gap-4 rounded-t-lg bg-gray px-4 py-6 text-xs min-[500px]:rounded-l-lg min-[500px]:rounded-r-none min-[500px]:text-sm">
+          <div className="flex grow basis-[55%] flex-col gap-4 rounded-t-lg bg-gray px-4 py-6 text-xs min-[500px]:rounded-l-lg min-[500px]:rounded-r-none min-[500px]:text-sm">
             <div className="flex items-center">
               <div className="grow font-bold">
                 <div className="flex justify-between">
-                  <p className="uppercase text-black">xx99 mk ii</p>
-                  <p className="opacity-50">x1</p>
+                  <p className="uppercase text-black">{shortname}</p>
+                  <p className="opacity-50">x{quantity}</p>
                 </div>
-                <p className="opacity-50">2,999</p>
+                <p className="opacity-50">{formattedPrice}</p>
               </div>
-              <img
-                src={xx99mkiiThumb}
-                alt="headphones thumbnail"
-                className="order-first h-12 w-12"
-              />
+              <img src={image} alt="headphones thumbnail" className="order-first h-12 w-12" />
             </div>
-            <p className="border-t border-t-zinc-300 pt-4 text-center text-xs">
-              and 2 other item(s)
-            </p>
+            {checkout.length > 1 && (
+              <p className="border-t border-t-zinc-300 pt-4 text-center text-xs">
+                and {checkout.length - 1} other item(s)
+              </p>
+            )}
           </div>
-          <div className="flex flex-col justify-center gap-2 rounded-b-lg bg-almostBlack px-4 pt-4 pb-5 text-white min-[500px]:rounded-l-none min-[500px]:rounded-r-lg">
+          <div className="flex basis-[45%] flex-col justify-center gap-2 rounded-b-lg bg-almostBlack px-4 pt-4 pb-5 text-white min-[500px]:rounded-l-none min-[500px]:rounded-r-lg">
             <p className="uppercase opacity-50">grand total</p>
-            <p className="text-lg">$ 5, 446</p>
+            <p className="text-lg">{subTotal}</p>
           </div>
         </div>
-        <Link to="../" className="mt-12">
+        <Button link path="../" className="mt-12 w-full">
           Back to Home
-        </Link>
+        </Button>
       </div>
     </div>
   );
