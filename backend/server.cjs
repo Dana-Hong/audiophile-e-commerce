ea126d9db48ee99c6ef7e23f06813fc91255ce6e
Dana <dana.hong@hotmail.com>
2023-08-03 16:31:01 -0400

    Feat: add order and cart routes

--- backend/server.cjs
+++ backend/server.cjs
@@ -2,12 +2,16 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const userRoutes = require("./routes/user.cjs");
+const orderRoutes = require("./routes/order.cjs");
+const cartRoutes = require("./routes/cart.cjs");
 
 const app = express();
 
 app.use(express.json());
 // routes
 app.use("/api/user", userRoutes);
+app.use("/api/orders", orderRoutes);
+app.use("/api/cart", cartRoutes);
 
 app.get("/", (req, res) => {
   res.status(200).send("Hello");
