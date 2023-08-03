13fde8446dfaba3ce0e3754dfae711cd7402b74e
Dana <dana.hong@hotmail.com>
2023-08-03 16:51:20 -0400

    Add cart endpoint

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,10 @@ export default defineConfig({
         target: "http://localhost:5000",
         changeOrigin: true,
       },
+      "/api/cart": {
+        target: "http://localhost:5000",
+        changeOrigin: true,
+      },
     },
   },
 });
